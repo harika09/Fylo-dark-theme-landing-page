51cfd392de156124454b1cb0ae0e781ff31aaef9
Harold <harold.goldensky@gmail.com>
2020-11-18 21:27:51 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/* Prevent the button going to top */
+document.getElementById("validate__btn").addEventListener("click", function(event){
+   event.preventDefault() 
+});
+
+
+/* Validate Email */
+function validate(){
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email.match(pattern)){
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+    }
+    else{
+        form.classList.add("invalid");
+        form.classList.remove("valid");
+        text.innerHTML = ("Please enter a valid email address");
+        text.style.color = "red";
+        document.getElementById("email").style.boderColor = "red";
+    }
+    
+}
